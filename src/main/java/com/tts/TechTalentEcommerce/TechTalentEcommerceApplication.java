130c5da814041db47b6021ea78cb2158b9a610d0
Liz Schilb <eschilb@gmail.com>
2020-07-16 17:58:57 -0400

    First commit

---
+++ src/main/java/com/tts/TechTalentEcommerce/TechTalentEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.tts.TechTalentEcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTalentEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTalentEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/TechTalentEcommerce/TechTalentEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.TechTalentEcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTalentEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
